0efacbe0f1aa8658dec502ee46c0c5bee9bb27ba
Nana Kweku <nanakwekudev@gmail.com>
2022-04-16 20:01:08 +0000

    work on home page widgets

---
+++ src/pages/components/widgets/Widget.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './widget.scss'
+
+const Widget = () => {
+  return (
+    <div className='widget'>
+        <div className="left">left</div>
+        <div className="right">right</div>
+    </div>
+  )
+}
+
+export default Widget
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Sidebar from '../components/sidebar/Sidebar'
 import Navbar from '../components/navbar/Navbar'
+import Widget from '../components/widgets/Widget'
 import './home.scss'
 const Home = () => {
   return (
     <div className='home'>
       <Sidebar/>
+
       <div className="homeContainer">
-        <Navbar></Navbar>
-        homeContainer
+        <Navbar/> {/*Navbar Component*/}
+
+        <div className="widgets">{/*Widget Component*/}
+          <Widget/>{/*Widget Component*/}
+          <Widget/>{/*Widget Component*/}
+          <Widget/>{/*Widget Component*/}
+          <Widget/>{/*Widget Component*/}
+        </div>
       </div>
     </div>
   )
