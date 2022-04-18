108646d166103d890fd9058072684ad163765823
Nana Kweku <nanakwekudev@gmail.com>
2022-04-18 12:16:32 +0000

    widget component started

--- src/pages/components/widgets/Widget.jsx
+++ src/pages/components/widgets/Widget.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { KeyboardArrowUp } from '@mui/icons-material';
+import { PersonOutlined } from '@mui/icons-material';
 import './widget.scss'
 
 const Widget = () => {
   return (
     <div className='widget'>
-        <div className="left">left</div>
-        <div className="right">right</div>
+        <div className="left">
+          <span className="title">USERS</span>
+          <span className="counter">2134</span>
+          <span className="link">See all users</span>
+        </div>
+        <div className="right">
+          <div className="percentage positive">
+            <KeyboardArrowUp/>
+            20%
+          </div>
+
+          <PersonOutlined className='icon'/>
+        </div>
     </div>
   )
 }
